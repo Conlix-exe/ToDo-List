79bd594f4c197883f52ee7252e80b1c09d4bc604
Conlix <conlix.pro@gmail.com>
2021-04-27 16:33:58 +0200

    Update SectionsPagerAdapter.java

--- app/src/main/java/com/example/todo_list/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/todo_list/ui/main/SectionsPagerAdapter.java
@@ -17,7 +17,7 @@ import com.example.todo_list.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.household, R.string.main, R.string.school, R.string.programming};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -41,6 +41,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         // Show 2 total pages.
-        return 2;
+        return 4;
     }
 }
