ccad31bcbbdfdc300c6e627cd3daeb57ab882ce0
Conlix <conlix.pro@gmail.com>
2021-05-04 08:03:47 +0200

    Update SectionsPagerAdapter.java

--- app/src/main/java/com/example/todo_list/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/todo_list/ui/main/SectionsPagerAdapter.java
@@ -29,6 +29,12 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
+        switch (position){
+            case 0:
+                return PlaceholderFragment.newInstance(position + 1);
+            case 1:
+                return MainFrag.newInstance(position + 1);
+        }
         return PlaceholderFragment.newInstance(position + 1);
     }
 
